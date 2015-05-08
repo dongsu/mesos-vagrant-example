c85c026fbcb43f1c7970f88b8a60503a008dd361
Lee, Dongsu <DLee3@DIRECTV.com>
2015-05-08 16:02:06 -0700

    adding marathon, chronos services on slave node to install open-jdk

--- init-slave.sh
+++ init-slave.sh
@@ -4,7 +4,7 @@ echo "start on manual" > /etc/init/mesos-master.override
 echo "start on manual" > /etc/init/chronos.override 
 echo "start on manual" > /etc/init/marathon.override 
 
-yum -y install mesos
+yum -y install mesos marathon chronos
 echo "zk://mesos-master:2181/mesos" > /etc/mesos/zk
 
 start mesos-slave
